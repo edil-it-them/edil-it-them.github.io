a5a324d103b6a027502a2fd892106184b6120c07
rolikasi <bayizbekovedil@gmail.com>
2021-03-15 14:13:29 +0200

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -17,8 +17,8 @@
   <meta property="og:type" content="website" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/Rolikasi/rolikasi.github.io/master/public/assets/img/ogimage.jpg" />
-  <meta property="og:image" content="https://github.com/Rolikasi/rolikasi.github.io/blob/master/public/assets/img/ogimage.jpg" />
+  <meta property="og:image" content="https://rolikasi.github.io/assets/img/ogimage.jpg" />
+  <meta property="og:image:secure_url" content="https://rolikasi.github.io/assets/img/ogimage.jpg" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1490" />
   <meta property="og:image:height" content="928" />
@@ -28,7 +28,7 @@
   <meta name="twitter:creator" content={hed} />
   <meta name="twitter:title" content={hed} />
   <meta name="twitter:description" content="I'm exploring new ways to tell stories in digital space with code. Let's collab!" />
-  <meta name="twitter:image:src" content="https://raw.githubusercontent.com/Rolikasi/rolikasi.github.io/master/public/assets/img/ogimage.jpg" />
+  <meta name="twitter:image:src" content="https://rolikasi.github.io/assets/img/ogimage.jpg" />
 
   <meta name="robots" content="max-image-preview:large" />
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏽‍💻</text></svg>">
