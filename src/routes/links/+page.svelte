cc3bafce5e5a6250bc55c9e6b80c92d59f434c6d
Edil <baiyzbekovedil@gmail.com>
2023-07-13 21:37:03 +0600

    add links page

---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+	let links = [
+		{
+			name: 'REGULARDISCO',
+			url: 'https://soundcloud.com/gasolineradio/regulardisco-w-edil-itthem-02072023'
+		},
+		{
+			name: 'daydream & heal',
+			url: 'https://refugeworldwide.com/radio/daydream-and-heal-edil-it-them-12-jan-2023'
+		},
+		{
+			name: 'razyob-and-pizdyachka',
+			url: 'https://soundcloud.com/edil-it-them/razyob-and-pizdyachka'
+		},
+		{ name: 'afterglow practice', url: 'https://soundcloud.com/edil-it-them/afterglow-practice' }
+	];
+</script>
+
+<div class="flex flex-col h-full mx-4">
+	<div class="flex flex-col max-w-md mx-auto text-center text-white my-auto w-full">
+		<h2
+			class="text-2xl font-bold animated-gradient-text_foreground__PuOdy animated-gradient-text_foreground-1__s2FIH"
+		>
+			DJ shows
+		</h2>
+		{#each links as link}
+			<a
+				href={link.url}
+				target="_blank"
+				class="bg-white hover:bg-opacity-90 text-black rounded-md hover:scale-105 transition-all
+            my-4 w-full py-2 px-2 shadow-orange shadow-lg"
+				>{link.name}
+			</a>
+		{/each}
+	</div>
+</div>
+
+<style>
+	/* .animated-gradient-text_foreground__PuOdy {
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		background-image: linear-gradient(90deg, #ffffff00, #f56d20);
+		position: relative;
+		z-index: 1;
+	}
+	.animated-gradient-text_foreground-1__s2FIH {
+		animation: animated-gradient-text_fade-foreground-1__3rW7l 8s infinite;
+	}
+	@keyframes animated-gradient-text_fade-foreground-1__3rW7l {
+		0%,
+		to {
+			background-image: linear-gradient(90deg, #ffffff00, #f56d20);
+		}
+
+		50% {
+			background-image: linear-gradient(90deg, #ffffff00, #f56d20);
+		}
+	} */
+</style>
